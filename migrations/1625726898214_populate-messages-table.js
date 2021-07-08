e4615e8993c7b3a5d750e8ec1b9241bd30dd7b21
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-08 09:13:47 +0200

    Corrected incorrect table name for population migration

--- migrations/1625726898214_populate-messages-table.js
+++ migrations/1625726898214_populate-messages-table.js
@@ -4,7 +4,7 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   pgm.sql(`
-  INSERT INTO users (title, text) VALUES
+  INSERT INTO messages (title, text) VALUES
   ('Express', 'Express is great for setting up servers.'),
   ('TypeScript', 'TypeScript is boosts the potential of Javascript.'),
   ('NodeJS', 'We went from browser to anywhere!');
