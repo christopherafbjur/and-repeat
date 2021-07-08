c55d9e8f2af0392372258bc4e61bb902602e5266
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-08 15:03:15 +0200

    Installed dep helmet and added error handler middleware to server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, {Application, } from 'express';
+import express, {Application } from 'express';
+import helmet from 'helmet';
 import messages from './routes/messages'
+import errorHandler from './middleware/errorHandler';
 
 const PORT: number = 5000;
 const app: Application = express();
 
+app.use(helmet())
 app.use(express.json());
+app.use(errorHandler)
 app.use('/', messages)
 
 
