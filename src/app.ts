6fc221891d11f4c3695f735895098c102c5a2835
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-08 09:28:47 +0200

    Added db query call to GET endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, {Application, Request, Response} from 'express';
+import db from './services/database';
 
 const PORT: number = 5000;
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('GET');
+  const query = `
+    SELECT * FROM messages;
+  `;
+
+  db.query(query, function (err, result) {
+    if (err) throw err;
+    res.status(200).send(result.rows);
+  });
 });
 
 app.post('/', (req: Request, res: Response) => {
